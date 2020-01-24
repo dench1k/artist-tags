3a8628968cc8a2890197893a6bf53f49623ca4fe
Den Moskvitin <denmoskvitin@gmail.com>
2020-01-24 22:09:34 +0200

    Add getUniqueArray

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,12 +1,20 @@
 const app = () => {
   // cached DOM
   const tracklistInput = document.querySelector(".js-tracklist-input");
+  const tracklistOutput = document.querySelector(".js-tracklist-output");
   const tracklistSubmit = document.querySelector(".js-tracklist-submit");
 
   //functions
   const submitTracklist = () => {
     const tracklistInputValue = tracklistInput.value;
-    console.log(tracklistInputValue);
+    const tracklistInputArray = tracklistInputValue.split("-");
+
+    const getUniqueArray = arr => {
+      return [...new Set(arr.sort((a, b) => a - b))];
+    };
+
+    const tracklistInputArrayUnique = getUniqueArray(tracklistInputArray);
+    console.log(tracklistInputArrayUnique);
   };
 
   tracklistSubmit.addEventListener("click", submitTracklist, false);
